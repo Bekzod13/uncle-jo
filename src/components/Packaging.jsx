68bfbf144bfc082c40d6e6a5d13a766a73c49ac9
Bekzod13 <bekzodjonsharobiddinov@gmail.com>
2023-04-27 10:16:06 +0500

    imges and form bugs have been fixed

--- src/components/Packaging.jsx
+++ src/components/Packaging.jsx
@@ -1,4 +1,6 @@
 import img from '../assets/packaging/img.png'
+import img1 from '../assets/packaging/img2.png'
+import img2 from '../assets/packaging/img3.png'
 
 const Packaging = () => {
     return (
@@ -15,12 +17,12 @@ const Packaging = () => {
                         <div className="packaging_main_product">
                             <p className='packaging_main_product_title packaging_main_product_title2'>Трехслойный пупырчатый стретч</p>
                             <p className='packaging_main_product_subtitle'>Для хрупĸого товара</p>
-                            <img src={img} alt="" />
+                            <img src={img1} alt="" />
                             <button className='img_btn img_btn_scale'>От 3000 сум/товар</button>
                         </div>
                         <div className="packaging_main_product">
                             <p className='packaging_main_product_title packaging_main_product_title2'>Большой ассортимент паĸетов</p>
-                            <img src={img} alt="" />
+                            <img src={img2} alt="" />
                             <button className='img_btn img_btn_scale'>От 700 сум/товар</button>
                         </div>
                     </div>
