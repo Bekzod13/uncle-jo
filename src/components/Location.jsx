30e092cf5c1a9c1ab3a9383ad5b29c75bd0b9094
Bekzod13 <bekzodjonsharobiddinov@gmail.com>
2023-04-28 09:14:17 +0500

    location image has been added to map

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
 import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
 
 const Location = () => {
-    const defaultState = {
+
+    const mapState = {
         center: [41.324940, 69.249526],
         zoom: 15,
       };
+
   return (
   
         <YMaps>
-            <Map width={'100%'} height={'100%'} defaultState={defaultState}>
-                <Placemark geometry={[41.324940, 69.249526]} />
+            <Map 
+                width={'100%'} 
+                height={'100%'} 
+                state={mapState}
+                modules={["layout.ImageWithContent"]}
+                >
+                <Placemark 
+                    geometry={[41.324940, 69.249526]}  
+                    options={{
+                        iconLayout: "default#imageWithContent",
+                        iconImageHref:
+                        "https://pbe-react-yandex-maps.vercel.app/img/logo.svg",
+                        iconImageSize: [40, 40]
+                    }}
+                 />
             </Map>
         </YMaps>
   );
